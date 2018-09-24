996638712172b15752b0bf5a46a0338c933479af
waldothedeveloper <w86compositor@gmail.com>
2018-09-24 00:53:29 -0400

    Added some css and changed the title

--- src/Root.js
+++ src/Root.js
@@ -7,7 +7,7 @@ class Root extends Component {
 		return (
 			<div className="App">
 				<div className="AppTitleContainer">
-					<h1 className="AppTitle">Awesome TODO App</h1>
+					<h1 className="AppTitle">Just do it</h1>
 				</div>
 				<InputForm />
 			</div>
